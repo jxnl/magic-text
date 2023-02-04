b45db55c0ca054dbe6e2a1f31571905fb5f407e2
Jason Liu <jason@jxnl.coA>
2023-02-04 10:46:00 -0500

    add a head

---
+++ magic-text/src/app/demo/sql/head.tsx
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <title>Magic SQL</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content="Ask your new AI Analyist." />
+      <meta property="og:description" content="Ask your new AI Analyist" />
+      <meta property="og:title" content="Ask your new AI Analyist" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Ask your new AI Analyist" />
+      <meta name="twitter:description" content="Ask your new AI Analyist" />
+    </>
+  );
+}
