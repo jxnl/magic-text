92d6d1534db2d289b65939904284e9d6ff45a06e
Jason Liu <jason@jxnl.coA>
2023-02-04 10:42:19 -0500

    "

--- magic-text/src/app/demo/sql/page.tsx
+++ magic-text/src/app/demo/sql/page.tsx
@@ -101,8 +101,8 @@ CREATE TABLE Products (
       <p className="text-md text-gray-600 my-6">
         Use the default schema or add your own. Ask questions in natural
         language and let Magic SQL do its best to explain the answer and suggest
-        data if it doesn't know. You can also Magic to add tables or columns and
-        the schema will automatically update.
+        data if it doesn{"'"}t know. You can also Magic to add tables or columns
+        and the schema will automatically update.
       </p>
       <textarea
         value={schema}
