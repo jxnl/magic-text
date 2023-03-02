3417351165b41d30a6a5732db204e3577b3f0c1c
Jason Liu <jason@jxnl.coA>
2023-03-02 11:57:13 -0500

    force head tags

--- magic-text/src/app/demo/youtube/head.tsx
+++ magic-text/src/app/demo/youtube/head.tsx
@@ -1,24 +1,29 @@
-export const desc = "Summarize a youtube video in real time.";
-export const title = "Magic Youtube";
-
 export default function Head() {
   return (
     <>
-      <title>{title}</title>
+      <title>Magic Youtube</title>
       <link rel="icon" href="/favicon.ico" />
-      <meta name="description" content={desc} />
-      <meta property="og:description" content={desc} />
-      <meta property="og:title" content={desc} />
       <meta
-        property="og:image"
-        content="https://www.theindianwire.com/wp-content/uploads/2017/08/youtube_logo_new_official_1504077880072.jpg"
+        name="description"
+        content="Turn video content into a mBringing together the worlds of video content and note-taking, Magic YouTube is the ultimate tool for extracting knowledge from online resources."
       />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={desc} />
+      <meta
+        property="og:description"
+        content="Bringing together the worlds of video content and note-taking, Magic YouTube is the ultimate tool for extracting knowledge from online resources."
+      />
+      <meta
+        property="og:title"
+        content="Bringing together the worlds of video content and note-taking, Magic YouTube is the ultimate tool for extracting knowledge from online resources."
+      />
+      <meta name="twitter:title" content="Magic Youtube" />
+      <meta
+        name="twitter:description"
+        content="Bringing together the worlds of video content and note-taking, Magic YouTube is the ultimate tool for extracting knowledge from online resources."
+      />
       <meta name="twitter:card" content="summary_large_image"></meta>
       <meta
         name="twitter:image"
-        content="https://www.theindianwire.com/wp-content/uploads/2017/08/youtube_logo_new_official_1504077880072.jpg"
+        content="https://cdn.discordapp.com/attachments/990816889657778196/1071941320052047953/CJ_EatinStarz_knight_Hacking_on_computer_matrix_071dc515-5af4-4911-a45f-2c2638863719.png"
       ></meta>
     </>
   );
