f5454757153a1a1ce0b84573059dbb500918daf4
Jason <jason@jxnl.co>
2023-07-25 23:57:28 +0800

    rushed: didnt fix import

--- magic-text/src/app/citations/components/CitationDisplay.tsx
+++ magic-text/src/app/citations/components/CitationDisplay.tsx
@@ -1,51 +1,67 @@
-import {ICitationData, ICitationLocation} from "@/app/api/exact_citations/route";
-import {useMemo, useState} from "react";
+import { useMemo, useState } from "react";
 
-const Citation = ({citation, onHover}: {
-  citation: ICitationData,
-  onHover: (location: ICitationLocation[]) => void
+type ICitationLocation = [number, number];
+
+type ICitationData = {
+  body: string;
+  spans: ICitationLocation[];
+  citation: string[];
+};
+
+const Citation = ({
+  citation,
+  onHover,
+}: {
+  citation: ICitationData;
+  onHover: (location: ICitationLocation[]) => void;
 }) => {
   return (
     <span
       onMouseEnter={() => onHover(citation.spans)}
       onMouseLeave={() => onHover([])}
       className="cursor-pointer block"
-    >{citation.body}</span>
-  )
-}
+    >
+      {citation.body}
+    </span>
+  );
+};
 
-const CitationDisplay = ({context, citations}: {
-  context: string,
-  citations: ICitationData[],
+const CitationDisplay = ({
+  context,
+  citations,
+}: {
+  context: string;
+  citations: ICitationData[];
 }) => {
-  const [highlight, setHighlight] = useState<ICitationLocation[]>([])
+  const [highlight, setHighlight] = useState<ICitationLocation[]>([]);
 
   const highlightedContext = useMemo(() => {
     if (highlight.length === 0) {
-      return context
+      return context;
     }
-    let result = context
+    let result = context;
     for (let loc of highlight) {
-      result = result.slice(0, loc[0]) + "<mark>" + result.slice(loc[0], loc[1]) + "</mark>" + result.slice(loc[1])
+      result =
+        result.slice(0, loc[0]) +
+        "<mark>" +
+        result.slice(loc[0], loc[1]) +
+        "</mark>" +
+        result.slice(loc[1]);
     }
-    return result
-  }, [context, highlight])
+    return result;
+  }, [context, highlight]);
 
   return (
     <div className="border-2 border-b border-transparent p-3">
       <div
         className="pb-2 text-gray-900 placeholder:text-gray-900 sm:text-sm sm:leading-6 font-mono"
-        dangerouslySetInnerHTML={{__html: highlightedContext}}
+        dangerouslySetInnerHTML={{ __html: highlightedContext }}
       />
-      {citations.map(c => (
-        <Citation
-          key={c.body}
-          citation={c}
-          onHover={setHighlight}
-        />
+      {citations.map((c) => (
+        <Citation key={c.body} citation={c} onHover={setHighlight} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export {CitationDisplay}
+export { CitationDisplay };
