f27771e7af0206692630176f048f7e3d993f87cd
Jason <jason@jxnl.co>
2023-07-26 22:22:46 +0800

    update styling

--- magic-text/src/app/citations/page.tsx
+++ magic-text/src/app/citations/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import {useRef, useState} from "react";
+import { useRef, useState } from "react";
 import "react-toastify/dist/ReactToastify.css";
-import {ToastContainer} from "react-toastify";
-import {CitationDisplay} from "@/app/citations/components/CitationDisplay";
+import { ToastContainer, toast } from "react-toastify";
+import { CitationDisplay } from "@/app/citations/components/CitationDisplay";
 import {
   Tabs,
   TabsContent,
   TabsList,
   TabsTrigger,
 } from "@/app/components/ui/tabs";
-import {Loader2} from "lucide-react";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
 
 const defaultContext =
   "My name is Jason Liu, and I grew up in Toronto Canada but I was born in China.I went to an arts highschool but in university I studied Computational Mathematics and physics.  As part of coop I worked at many companies including Stitchfix, Facebook.  I also started the Data Science club at the University of Waterloo and I was the president of the club for 2 years.";
@@ -50,34 +51,52 @@ export default function Example() {
         query: inputRef.current!.value,
         context,
       }),
-    }).then(async response => {
-      const reader = response.body!.getReader()
-      const decoder = new TextDecoder();
+    })
+      .then(async (response) => {
+        const reader = response.body!.getReader();
+        const decoder = new TextDecoder();
 
-      async function read(): Promise<any> {
-        const {done, value} = await reader.read();
+        async function read(): Promise<any> {
+          const { done, value } = await reader.read();
 
-        if (done) {
-          return;
+          if (done) {
+            return;
+          }
+          console.log("received", value);
+          const chunk = decoder.decode(value);
+          console.log("decoded", chunk);
+          const parsedCitation: ICitationData = JSON.parse(chunk);
+          setCitations((prev) => Array.from(prev).concat(parsedCitation));
+          return read();
         }
-        console.log('received', value)
-        const chunk = decoder.decode(value);
-        console.log('decoded', chunk)
-        const parsedCitation: ICitationData = JSON.parse(chunk)
-        setCitations(prev => Array.from(prev).concat(parsedCitation))
-        return read();
-      }
 
-      await read();
-    }).finally(() => setLoading(false))
+        await read();
+      })
+      .finally(() => {
+        setLoading(false);
+        toast.success("Try hovering over the statements");
+      });
   };
 
   return (
     <>
-      <ToastContainer/>
+      <ToastContainer />
       <div className="mx-auto max-w-7xl lg:flex lg:gap-8 lg:px-8">
-        <div className="px-6 pt-10 pb-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pt-20 lg:pb-56 xl:col-span-6 flex-1">
+        <div className="px-6 pt-10 pb-10 sm:pb-22 lg:col-span-7 lg:px-0 lg:pt-20 lg:pb-56 xl:col-span-6 flex-1">
           <div className="mx-auto max-w-2xl lg:mx-0">
+            <div className="hidden sm:mb-8 sm:flex sm:justify-left">
+              <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                Powered by OpenAI Function calls.{" "}
+                <Link
+                  target="_blank"
+                  href="https://wandb.ai/jxnlco/function-calls/reports/Better-Data-Extraction-Using-Pydantic-and-OpenAI-Function-Calls--Vmlldzo0ODU4OTA3"
+                  className="font-semibold text-red-600"
+                >
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  Read more <span aria-hidden="true">&rarr;</span>
+                </Link>
+              </div>
+            </div>
             <h1 className="mt-12 text-4xl font-bold tracking-tight text-gray-900 sm:mt-10 sm:text-6xl">
               Exact citations
             </h1>
@@ -111,7 +130,7 @@ export default function Example() {
                 type="button"
                 className="rounded-md flex items-center bg-red-600 py-2 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 disabled:opacity-80"
               >
-                {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin"/>}
+                {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
                 {loading ? "Generating..." : "Go!"}
               </button>
             </div>
@@ -148,7 +167,7 @@ export default function Example() {
               </div>
             </TabsContent>
             <TabsContent value="preview">
-              <CitationDisplay context={savedContext} citations={citations}/>
+              <CitationDisplay context={savedContext} citations={citations} />
             </TabsContent>
           </Tabs>
         </div>
